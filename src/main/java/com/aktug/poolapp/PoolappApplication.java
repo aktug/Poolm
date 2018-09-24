2f7183ac44b94e265c1ba738e8a101776f4f051e
Aktuğ Urun <aktugurun@gmail.com>
2018-09-24 22:37:02 +0300

    Init

---
+++ src/main/java/com/aktug/poolapp/PoolappApplication.java
@@ -0,0 +1,12 @@
+package com.aktug.poolapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoolappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PoolappApplication.class, args);
+    }
+}
